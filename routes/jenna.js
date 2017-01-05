dff0a2709ff279ebf0b3daee032cb4af6c92ae64
Alex Chen <chennosaurus@Alexs-MacBook-Pro-2.local>
2017-01-04 22:18:03 -0800

    updated with some easter eggs

--- routes/jenna.js
+++ routes/jenna.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('users');
+router.get('/', (req, res, next) => {
+    res.render('jenna');
 });
 
 module.exports = router;
