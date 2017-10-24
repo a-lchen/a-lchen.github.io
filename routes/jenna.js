6c1912a05ea302a1d75ef18b4d6609cdbd7a179d
Alex Chen <achen19@mit.edu>
2017-10-24 00:11:16 -0400

    fix

--- routes/jenna.js
+++ routes/jenna.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient
 
 
 router.get('/', (req, res, next) => {
